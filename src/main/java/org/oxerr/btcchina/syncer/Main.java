7604b1d43b7d7e7cb76f9d9562ff05f3a36f2e6c
Joe Zhou <zhoushuqun@gmail.com>
2015-05-09 16:44:02 +0800

    Remove java.util.logging.manager setting from the main method as we have set it in the start script.

--- src/main/java/org/oxerr/btcchina/syncer/Main.java
+++ src/main/java/org/oxerr/btcchina/syncer/Main.java
@@ -5,7 +5,6 @@ import org.apache.commons.daemon.support.DaemonLoader;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
 		DaemonLoader.load(SyncerDaemon.class.getName(), args);
 		DaemonLoader.start();
 	}
