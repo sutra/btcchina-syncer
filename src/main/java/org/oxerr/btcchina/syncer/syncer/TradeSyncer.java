50e184bebe75a7531dc6b65997e8ffa7246431e1
Joe Zhou <zhoushuqun@gmail.com>
2015-06-05 15:18:21 +0800

    Change TraderSyncer to be trigger by spring task scheduler.

--- src/main/java/org/oxerr/btcchina/syncer/syncer/TradeSyncer.java
+++ src/main/java/org/oxerr/btcchina/syncer/syncer/TradeSyncer.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+
 import org.oxerr.btcchina.syncer.dao.TradeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,34 +16,28 @@ import com.xeiam.xchange.btcchina.dto.marketdata.BTCChinaTrade;
 import com.xeiam.xchange.btcchina.service.polling.BTCChinaMarketDataServiceRaw;
 
 @Component
-public class TradeSyncer extends AbstractSyncer {
+public class TradeSyncer {
 
 	private final Logger log = Logger.getLogger(TradeSyncer.class.getName());
 
 	private final BTCChinaMarketDataServiceRaw rawMdService;
 	private final TradeDao tradeDao;
 	private final String market;
-	private volatile int limit = 5_000;
+	private final int limit = 5_000;
 	private long lastId;
 
 	@Autowired
 	public TradeSyncer(
 			BTCChinaMarketDataServiceRaw rawMdService,
 			TradeDao tradeDao,
-			@Value("${btcchina.trade.interval}") long interval,
 			@Value("${btcchina.trade.market}") String market) {
-		super(interval);
 		this.rawMdService = rawMdService;
 		this.tradeDao = tradeDao;
 		this.market = market;
 	}
 
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
-
-	@Override
-	protected void init() {
+	@PostConstruct
+	private void init() {
 		lastId = tradeDao.getLastId();
 		if (lastId == 0) {
 			// If pass 0, BTCChina will return the recent trades that is not
@@ -51,8 +47,7 @@ public class TradeSyncer extends AbstractSyncer {
 		log.log(Level.FINE, "Last ID: {0}", lastId);
 	}
 
-	@Override
-	protected void sync() throws IOException {
+	public void sync() throws IOException {
 		BTCChinaTrade[] trades;
 		do {
 			log.log(Level.INFO, "Last ID: {0}", lastId);
